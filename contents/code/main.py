c505c1196d94b48c97787e708ce89ed84b4ec7eb
Erlend Hamberg <ehamberg@gmail.com>
2009-05-06 22:57:11 +0200

    works!

--- contents/code/main.py
+++ contents/code/main.py
@@ -1,14 +1,26 @@
+import urllib
+import re
+
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+
+from PyKDE4.kdecore import *
+from PyKDE4.kdeui import *
+from PyKDE4.kio import *
+
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
-from PyKDE4.kdecore import KUrl
+#from PyKDE4.kdecore import KUrl
 
-class HelloWorldApplet(plasmascript.Applet):
+from settings import Settings
+
+class GCalApplet(plasmascript.Applet):
     def __init__(self,parent,args=None):
         plasmascript.Applet.__init__(self,parent)
+        self.parent = parent
 
     def init(self):
+        self.settings = {}
         self.setHasConfigurationInterface(False)
 
         self.theme = Plasma.Svg(self)
@@ -16,12 +28,97 @@ class HelloWorldApplet(plasmascript.Applet):
         self.setBackgroundHints(Plasma.Applet.DefaultBackground)
         self.setAspectRatioMode(Plasma.IgnoreAspectRatio)
 
+        self.wallet = KWallet.Wallet.openWallet(KWallet.Wallet.LocalWallet(), 0)
+
+        src = ""
+
+        if self.wallet <> None:
+            if not self.wallet.hasFolder("gcal-plasmoid"):
+                self.wallet.createFolder("gcal-plasmoid")
+
+            self.wallet.setFolder("gcal-plasmoid")
+            if self.wallet.entryList().isEmpty():
+                self.showConfigurationInterface()
+
+            username = str(self.wallet.entryList().first()) # FIXME
+
+            password = QString()
+            self.wallet.readPassword(username, password)
+
+            self.storeUserAndDomain(username, str(password))
+            src = self.getSrc()
+
+        else:
+            src = "KWallet FAIL"
+
         self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
-        webView = Plasma.WebView(self.applet)
-        webView.setUrl(KUrl("http://google.com/calendar/m"))
-        self.layout.addItem(webView)
+        self.webview = Plasma.WebView(self.applet)
+        #self.webview.setUrl(KUrl("http://google.com/calendar/m"))
+        self.webview.setHtml(src)
+        self.layout.addItem(self.webview)
         self.setLayout(self.layout)
         self.resize(300,400)
 
+    def createConfigurationInterface(self, parent):
+        self.gcalsettings = Settings(self)
+        p = parent.addPage(self.gcalsettings, i18n("Google Calendar Settings") )
+
+        self.connect(parent, SIGNAL("okClicked()"), self.configAccepted)
+        self.connect(parent, SIGNAL("cancelClicked()"), self.configDenied)
+
+    def showConfigurationInterface(self):
+        dialog = KPageDialog()
+        dialog.setFaceType(KPageDialog.Plain)
+        dialog.setButtons( KDialog.ButtonCode(KDialog.Ok | KDialog.Cancel) )
+        self.createConfigurationInterface(dialog)
+        dialog.resize(400,300)
+        dialog.exec_()
+
+    def configDenied(self):
+        pass
+
+    def configAccepted(self):
+        self.settings = self.gcalsettings.getSettings()
+        self.storeUserAndDomain(self.settings['username'], self.settings['password'])
+        wallet = KWallet.Wallet.openWallet(KWallet.Wallet.LocalWallet(), 0)
+        if wallet <> None:
+            wallet.setFolder("gcal-plasmoid")
+            for e in self.wallet.entryList():
+                wallet.removeEntry(e)
+            wallet.writePassword(QString(self.settings['username']),
+                    QString(self.settings['password']))
+            wallet = None
+
+        self.webview.setHtml(self.getSrc())
+
+    def storeUserAndDomain(self, username, password):
+        re_email = re.compile('@(.+)')
+        m = re_email.search(username)
+
+        # if username is an email address with a domain name other than
+        # gmail.com, assume it's a Google Apps for your Domain thingie and
+        # change the url
+        if m <> None and m.group(1) <> "gmail.com":
+            self.url = "http://google.com/calendar/hosted/" + m.group(1) + "/m"
+            username = re_email.sub('', username)
+        else:
+            self.url = "http://google.com/calendar/m"
+
+        self.settings['username'] = username
+        self.settings['password'] = password
+
+    def getSrc(self):
+        webFile = urllib.urlopen(self.url)
+        src = webFile.read()
+        webFile.close()
+
+        src = src.replace("id=\"Email\"", "id=\"Email\" value=\""+self.settings["username"]+"\"")
+        src = src.replace("id=\"Passwd\"", "id=\"Passwd\" value=\""+self.settings["password"]+"\"")
+        src = src.replace("id=\"gaia_loginform\"", "id=\"gaia_loginform\" name=\"gaia_loginform\"")
+        src = src.replace("</body>", "<script>document.gaia_loginform.submit()</script>\n</body>")
+
+        return src
+
+
 def CreateApplet(parent):
-    return HelloWorldApplet(parent)
+    return GCalApplet(parent)
--- contents/code/settings.py
+++ contents/code/settings.py
@@ -7,27 +7,26 @@ from PyKDE4.kdecore import *
 from settingsform import Ui_gcal_settings
 
 class Settings(QWidget, Ui_gcal_settings):
-    def __init__(self, parent, settings):
+    def __init__(self, parent):
         QWidget.__init__(self)
 
-        self.wallet = KWallet.Wallet.openWallet(KWallet.Wallet.LocalWallet(), 0)#, 1)
-        if self.wallet <> None:
-            self.connect(self.wallet, SIGNAL("walletOpened(bool)"), self.walletOpened)
-        else:
-            print(":(")
-            # KWallet service is disabled
-            #self.checkMail()
-            #self.timer.start(self.settings["pollinterval"] * 60000)
+        self.parent = parent
 
         self.setupUi(self)
+        self.wallet = KWallet.Wallet.openWallet(KWallet.Wallet.LocalWallet(), 0)#, 1)
 
-    def walletOpened(self, success):
-        if success:
-            print "Wallet opened"
-            # Get passwords from wallet
+        if self.wallet <> None:
             self.wallet.setFolder("gcal-plasmoid")
 
-            passwd = QString()
-            self.wallet.readPassword(self.settings["username"], passwd)
-            print(str(passwd))
-            self.settings["password"] = str(passwd)
+            if  not self.wallet.entryList().isEmpty():
+                username = str(self.wallet.entryList().first())
+                password = QString()
+                self.wallet.readPassword(username, password)
+                self.le_email.setText(username)
+                self.le_password.setText(str(password))
+        else:
+            print(":(")
+
+    def getSettings(self):
+        return {'username': str(self.le_email.text()),
+                'password': str(self.le_password.text())}
